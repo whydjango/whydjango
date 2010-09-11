4a0daaff26221c41d57258d8b887853bee2d8cda
Audrey M Roy <audreyr@gmail.com>
2010-09-11 16:56:26 -0700

    Adding live production server config info

--- deployment.py
+++ deployment.py
@@ -10,6 +10,13 @@ settings = {
         'buildout_cfg': 'server_dev.cfg',
         'backup_dir': '/home/jonas/bk/ydjango/',
     },
+    'live': {
+        'hosts': ['whydjango.com'],
+        'path': '/home/whydjango/apps/ydjango/',
+        'user': 'whydjango',
+        'buildout_cfg': 'server_live.cfg',
+        'backup_dir': '/home/whydjango/bk/ydjango/',
+    },
 }
 for key, value in settings.items():
     for k,v in defaults.items():
--- py_src/project/settings/server_live.py
+++ py_src/project/settings/server_live.py
@@ -1,1 +1,6 @@
 from project.settings.base import *
+
+CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
+
+CMS_CACHE_PREFIX = 'ydjango-dev'
+
